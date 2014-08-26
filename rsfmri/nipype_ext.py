c74b1fee6fd32e048f6b30903c985d2603802a31
Katelyn Begany <kbegany@nx6.priv>
2014-08-26 12:46:50 -0700

    NF: add SPM and Dartel interface to nipype_ext

--- rsfmri/nipype_ext.py
+++ rsfmri/nipype_ext.py
@@ -212,6 +212,154 @@ class RealignUnwarp(SPMCommand):
         return outputs
 
 
+class NewSegmentInputSpec(SPMCommandInputSpec):
+    channel_files = InputMultiPath(File(exists=True),
+                              desc="A list of files to be segmented",
+                              field='channel', copyfile=False, mandatory=True)
+    channel_info = traits.Tuple(traits.Float(), traits.Float(),
+                                traits.Tuple(traits.Bool, traits.Bool),
+                                desc="""A tuple with the following fields:
+            - bias reguralisation (0-10)
+            - FWHM of Gaussian smoothness of bias
+            - which maps to save (Corrected, Field) - a tuple of two boolean values""", 
+            field='channel')
+    tissues = traits.List(traits.Tuple(traits.Tuple(File(exists=True), traits.Int()), traits.Int(),
+                                       traits.Tuple(traits.Bool, traits.Bool), traits.Tuple(traits.Bool, traits.Bool)),
+                         desc="""A list of tuples (one per tissue) with the following fields:
+            - tissue probability map (4D), 1-based index to frame
+            - number of gaussians
+            - which maps to save [Native, DARTEL] - a tuple of two boolean values
+            - which maps to save [Modulated, Unmodualted] - a tuple of two boolean values""", 
+            field='tissue')
+    affine_regularization = traits.Enum('mni', 'eastern', 'subj', 'none', field='warp.affreg',
+                      desc='mni, eastern, subj, none ')
+    warping_regularization = traits.Float(field='warp.reg',
+                      desc='Aproximate distance between sampling points.')
+    sampling_distance = traits.Float(field='warp.samp',
+                      desc='Sampling distance on data for parameter estimation')
+    write_deformation_fields = traits.List(traits.Bool(), minlen=2, maxlen=2, field='warp.write',
+                                           desc="Which deformation fields to write:[Inverse, Forward]")
+
+class NewSegmentOutputSpec(TraitedSpec):
+    native_class_images = traits.List(traits.List(File(exists=True)), desc='native space probability maps')
+    dartel_input_images = traits.List(traits.List(File(exists=True)), desc='dartel imported class images')
+    normalized_class_images = traits.List(traits.List(File(exists=True)), desc='normalized class images')
+    modulated_class_images = traits.List(traits.List(File(exists=True)), desc='modulated+normalized class images')
+    transformation_mat = OutputMultiPath(File(exists=True), desc='Normalization transformation')
+    bias_corrected_images = OutputMultiPath(File(exists=True), desc='bias corrected images')
+    bias_field_images = OutputMultiPath(File(exists=True), desc='bias field images')
+
+class NewSegment(SPMCommand):
+    """Use spm_preproc8 (New Segment) to separate structural images into different
+    tissue classes. Supports multiple modalities.
+
+    NOTE: This interface currently supports single channel input only
+    
+    http://www.fil.ion.ucl.ac.uk/spm/doc/manual.pdf#page=185
+
+    Examples
+    --------
+    >>> import nipype.interfaces.spm as spm
+    >>> seg = spm.NewSegment()
+    >>> seg.inputs.channel_files = 'structural.nii'
+    >>> seg.inputs.channel_info = (0.0001, 60, (True, True))
+    >>> seg.run() # doctest: +SKIP
+
+    For VBM pre-processing [http://www.fil.ion.ucl.ac.uk/~john/misc/VBMclass10.pdf],
+    TPM.nii should be replaced by /path/to/spm8/toolbox/Seg/TPM.nii
+
+    >>> seg = NewSegment()
+    >>> seg.inputs.channel_files = 'structural.nii'
+    >>> tissue1 = (('TPM.nii', 1), 2, (True,True), (False, False))
+    >>> tissue2 = (('TPM.nii', 2), 2, (True,True), (False, False))
+    >>> tissue3 = (('TPM.nii', 3), 2, (True,False), (False, False))
+    >>> tissue4 = (('TPM.nii', 4), 2, (False,False), (False, False))
+    >>> tissue5 = (('TPM.nii', 5), 2, (False,False), (False, False))
+    >>> seg.inputs.tissues = [tissue1, tissue2, tissue3, tissue4, tissue5]
+    >>> seg.run() # doctest: +SKIP
+
+    """
+
+    input_spec = NewSegmentInputSpec
+    output_spec = NewSegmentOutputSpec
+    _jobtype = 'tools'
+    _jobname = 'preproc8'
+
+    def _format_arg(self, opt, spec, val):
+        """Convert input to appropriate format for spm
+        """
+
+        if opt in ['channel_files', 'channel_info']:
+            # structure have to be recreated, because of some weird traits error
+            new_channel = {}
+            new_channel['vols'] = scans_for_fnames(self.inputs.channel_files)
+            if isdefined(self.inputs.channel_info):
+                info = self.inputs.channel_info
+                new_channel['biasreg'] = info[0]
+                new_channel['biasfwhm'] = info[1]
+                new_channel['write'] = [int(info[2][0]), int(info[2][1])]
+            return [new_channel]
+        elif opt == 'tissues':
+            new_tissues = []
+            for tissue in val:
+                new_tissue = {}
+                new_tissue['tpm'] = np.array([','.join([tissue[0][0], str(tissue[0][1])])], dtype=object)
+                new_tissue['ngaus'] = tissue[1]
+                new_tissue['native'] = [int(tissue[2][0]), int(tissue[2][1])]
+                new_tissue['warped'] = [int(tissue[3][0]), int(tissue[3][1])]
+                new_tissues.append(new_tissue)
+            return new_tissues
+        elif opt == 'write_deformation_fields':
+            return [int(val[0]), int(val[1])]
+        elif opt == 'warping_regularization':
+            return int(val)
+        elif opt == 'affine_regularization':
+            return val
+            
+            
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        outputs['native_class_images'] = []
+        outputs['dartel_input_images'] = []
+        outputs['normalized_class_images'] = []
+        outputs['modulated_class_images'] = []
+        outputs['transformation_mat'] = []
+        outputs['bias_corrected_images'] = []
+        outputs['bias_field_images'] = []
+
+        n_classes = 5
+        if isdefined(self.inputs.tissues):
+            n_classes = len(self.inputs.tissues)
+        for i in range(n_classes):
+            outputs['native_class_images'].append([])
+            outputs['dartel_input_images'].append([])
+            outputs['normalized_class_images'].append([])
+            outputs['modulated_class_images'].append([])
+
+        for filename in self.inputs.channel_files:
+            pth, base, ext = split_filename(filename)
+            if isdefined(self.inputs.tissues):
+                for i, tissue in enumerate(self.inputs.tissues):
+                    if tissue[2][0]:
+                        outputs['native_class_images'][i].append(os.path.join(pth,"c%d%s%s"%(i+1, base, ext)))
+                    if tissue[2][1]:
+                        outputs['dartel_input_images'][i].append(os.path.join(pth,"rc%d%s%s"%(i+1, base, ext)))
+                    if tissue[3][0]:
+                        outputs['normalized_class_images'][i].append(os.path.join(pth,"wc%d%s%s"%(i+1, base, ext)))
+                    if tissue[3][1]:
+                        outputs['modulated_class_images'][i].append(os.path.join(pth,"mwc%d%s%s"%(i+1, base, ext)))
+            else:
+                for i in range(n_classes):
+                    outputs['native_class_images'][i].append(os.path.join(pth,"c%d%s%s"%(i+1, base, ext)))
+            outputs['transformation_mat'].append(os.path.join(pth, "%s_seg8.mat" % base))
+            if isdefined(self.inputs.channel_info):
+                if self.inputs.channel_info[2][0]:
+                    outputs['bias_corrected_images'].append(os.path.join(pth, "m%s%s" % (base, ext)))
+                if self.inputs.channel_info[2][1]:
+                    outputs['bias_field_images'].append(os.path.join(pth, "BiasField_%s%s" % (base, ext)))
+        return outputs
+
 class VBM8InputSpec(SPMCommandInputSpec):
     in_files = traits.Either(traits.List(File(exists=True)),File(exists=True),
                              field='estwrite.data',
@@ -559,3 +707,38 @@ class DARTELNorm2MNI(SPMCommand):
                                                                           ext)))
 
         return outputs
+    
+
+if __name__ == '__main__':
+    infile = glob('/home/jagust/cindeem/CODE/manja/testdata/multi2/PIDN8886_*.nii')
+    infile.sort()
+    startdir = os.getcwd()
+    ru = RealignUnwarp(matlab_cmd = 'matlab-spm8')
+    ru.inputs.in_files = infile
+    
+    #ru.inputs.phase_map = '/home/jagust/cindeem/CODE/manja/corgunwarp.mat'
+    #mystr = ru._generate_job(contents = ru._parse_inputs()[0])
+    mscript = ru._make_matlab_command(deepcopy(ru._parse_inputs()))
+    print ru.fix_mscript(mscript)
+    
+    pth, _ = os.path.split(infile[0])
+    os.chdir(pth)
+    #ruout = ru.run()
+    
+    #os.chdir(startdir)
+
+    vbmfile = '/home/jagust/cindeem/CODE/manja/testdata/anatomy2_vbm8/B10-235.nii'
+    pth, _ = os.path.split(vbmfile)
+    os.chdir(pth)
+    vbm = VBM8(matlab_cmd = 'matlab-spm8')
+    vbm.inputs.in_files =[vbmfile]
+    vbm.inputs.write_warps = [True,True]
+    vbm.inputs.write_gm = [1,1, 1,2]
+    vbm.inputs.ngaus = [2,2,2,3,4,2]
+    inputs = vbm._parse_inputs()
+    mscript = vbm._make_matlab_command(deepcopy(inputs))
+    vbmout = vbm.run()
+    os.chdir(startdir)
+    #print 'mfile', ruout.interface.inputs.mfile
+    #print ruout.interface.mlab.inputs.script
+    #spm_jobman('interactive',jobs)
